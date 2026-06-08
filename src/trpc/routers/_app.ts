abf516088466cc686d77935f580c87fe8da513ec
Amanda Anderson <amanderson9@yahoo.com>
2026-06-07 23:27:55 -0400

    coderabbit suggestions made

---
+++ src/trpc/routers/_app.ts
@@ -0,0 +1,14 @@
+import { baseProcedure, createTRPCRouter } from "../init";
+export const appRouter = createTRPCRouter({
+  health: baseProcedure.query(async () => {
+    //Uncomment to demo Suspense loading state:
+    // await new Promise((resolve) => setTimeout(resolve, 5000))
+
+    // Uncomment to demo ErrorBoundray
+    // throw new Error("Something Went Wrong!")
+
+    return { status: "ok" };
+  }),
+});
+// export type definition of API
+export type AppRouter = typeof appRouter;
